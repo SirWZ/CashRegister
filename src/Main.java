a811fa97d86f5f644c45e06fc6ead97b9ecb308f
Yurii Gevtsi <y.gevtsi@gmail.com>
2018-07-12 18:52:10 +0200

    ff

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         UserInterface UI = new UserInterface();
         UI.setVisible(true);
-
-        //try
+        //iiii
     }
 }
