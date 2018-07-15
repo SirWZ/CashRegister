1482407ed43435ca5ccffe517ba41846c8e9e897
prof. Kondraki <prof. Kondraki@192.168.0.104>
2018-07-15 12:58:03 +0300

    Добавил ActionListener и кнопки на Выплату и вплату

--- src/Models/UserInterface.java
+++ src/Models/UserInterface.java
@@ -19,7 +19,7 @@ public class UserInterface extends JFrame {
     private void loggBtnActionPerformed() {
         new CashierViewWindow();
         this.setVisible(false);
-         new CashInOut("Выплата").setVisible(true);
+         //new CashInOut("Выплата").setVisible(true);
     }
 
     private void passwordFieldKeyPressed(KeyEvent e) {
