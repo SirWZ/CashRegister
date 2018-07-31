190c46b02809c68ef30fefaf8b4d009bb1e43e89
Yurii Gevtsi <y.gevtsi@gmail.com>
2018-07-31 15:07:34 +0200

    Login to BD

--- src/Models/UserInterface.java
+++ src/Models/UserInterface.java
@@ -62,7 +62,7 @@ public class UserInterface extends JFrame {
     }
 
     private void okBtnActionPerformed() {
-        new CashierViewWindow(cn);
+        moneydialog.setVisible(true);
         newworkdialog.dispose();
     }
 
@@ -79,9 +79,31 @@ public class UserInterface extends JFrame {
         }
     }
 
+    private void okbtnActionPerformed() {
+        moneydialog.dispose();printreptdialog.setVisible(true);
+       CashInOut cio = new CashInOut("Вплата");
+       cio.setVisible(true);
+
+
+    }
+
+    private void nobtnActionPerformed() {
+        moneydialog.dispose();
+        printreptdialog.setVisible(true);
+    }
+
+    private void noreptbtnActionPerformed() {
+        printreptdialog.dispose();
+        new CashierViewWindow(cn);
+    }
+
+    private void okreptbtnActionPerformed() {
+        noreptbtnActionPerformed();
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - Gevtsi Yurii
+        // Generated using JFormDesigner Evaluation license - Yurii
         JLabel loginLable = new JLabel();
         logintextField = new JTextField();
         JLabel passwordLabel = new JLabel();
@@ -95,6 +117,18 @@ public class UserInterface extends JFrame {
         JPanel panelbtn = new JPanel();
         JButton okBtn = new JButton();
         JButton noBtn = new JButton();
+        moneydialog = new JDialog();
+        panel1 = new JPanel();
+        moneylbl = new JLabel();
+        panel2 = new JPanel();
+        JButton okbtn = new JButton();
+        JButton nobtn = new JButton();
+        printreptdialog = new JDialog();
+        panel3 = new JPanel();
+        reportlbl = new JLabel();
+        panel4 = new JPanel();
+        JButton okreptbtn = new JButton();
+        JButton noreptbtn = new JButton();
 
         //======== this ========
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -205,14 +239,141 @@ public class UserInterface extends JFrame {
             newworkdialog.setSize(255, 145);
             newworkdialog.setLocationRelativeTo(newworkdialog.getOwner());
         }
+
+        //======== moneydialog ========
+        {
+            moneydialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            moneydialog.setResizable(false);
+            Container moneydialogContentPane = moneydialog.getContentPane();
+            moneydialogContentPane.setLayout(new GridBagLayout());
+            ((GridBagLayout)moneydialogContentPane.getLayout()).columnWidths = new int[] {0, 0};
+            ((GridBagLayout)moneydialogContentPane.getLayout()).rowHeights = new int[] {0, 0, 0};
+            ((GridBagLayout)moneydialogContentPane.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+            ((GridBagLayout)moneydialogContentPane.getLayout()).rowWeights = new double[] {1.0, 1.0, 1.0E-4};
+
+            //======== panel1 ========
+            {
+                panel1.setLayout(new GridBagLayout());
+                ((GridBagLayout)panel1.getLayout()).columnWidths = new int[] {14, 0, 0};
+                ((GridBagLayout)panel1.getLayout()).rowHeights = new int[] {0, 0};
+                ((GridBagLayout)panel1.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0E-4};
+                ((GridBagLayout)panel1.getLayout()).rowWeights = new double[] {1.0, 1.0E-4};
+
+                //---- moneylbl ----
+                moneylbl.setText("\u0412\u043d\u0435\u0441\u0442\u0438 \u0434\u0435\u043d\u044c\u0433\u0438?");
+                moneylbl.setFont(new Font("Segoe UI", Font.PLAIN, 30));
+                panel1.add(moneylbl, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            moneydialogContentPane.add(panel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 5, 0), 0, 0));
+
+            //======== panel2 ========
+            {
+                panel2.setLayout(new GridBagLayout());
+                ((GridBagLayout)panel2.getLayout()).columnWidths = new int[] {0, 0, 0};
+                ((GridBagLayout)panel2.getLayout()).rowHeights = new int[] {0, 0};
+                ((GridBagLayout)panel2.getLayout()).columnWeights = new double[] {1.0, 1.0, 1.0E-4};
+                ((GridBagLayout)panel2.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
+
+                //---- okbtn ----
+                okbtn.setText("\u0414\u0430");
+                okbtn.addActionListener(e -> okbtnActionPerformed());
+                panel2.add(okbtn, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- nobtn ----
+                nobtn.setText("\u041d\u0435\u0442");
+                nobtn.addActionListener(e -> nobtnActionPerformed());
+                panel2.add(nobtn, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            moneydialogContentPane.add(panel2, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
+            moneydialog.setSize(255, 135);
+            moneydialog.setLocationRelativeTo(moneydialog.getOwner());
+        }
+
+        //======== printreptdialog ========
+        {
+            printreptdialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            printreptdialog.setResizable(false);
+            printreptdialog.setAutoRequestFocus(false);
+            Container printreptdialogContentPane = printreptdialog.getContentPane();
+            printreptdialogContentPane.setLayout(new GridBagLayout());
+            ((GridBagLayout)printreptdialogContentPane.getLayout()).columnWidths = new int[] {0, 0};
+            ((GridBagLayout)printreptdialogContentPane.getLayout()).rowHeights = new int[] {0, 0, 0};
+            ((GridBagLayout)printreptdialogContentPane.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+            ((GridBagLayout)printreptdialogContentPane.getLayout()).rowWeights = new double[] {1.0, 1.0, 1.0E-4};
+
+            //======== panel3 ========
+            {
+                panel3.setLayout(new GridBagLayout());
+                ((GridBagLayout)panel3.getLayout()).columnWidths = new int[] {0, 0};
+                ((GridBagLayout)panel3.getLayout()).rowHeights = new int[] {0, 0};
+                ((GridBagLayout)panel3.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+                ((GridBagLayout)panel3.getLayout()).rowWeights = new double[] {1.0, 1.0E-4};
+
+                //---- reportlbl ----
+                reportlbl.setText("\u041d\u0430\u043f\u0435\u0447\u0430\u0442\u0430\u0442\u044c \u043e\u0442\u0447\u0435\u0442?");
+                reportlbl.setFont(new Font("Segoe UI", Font.PLAIN, 30));
+                panel3.add(reportlbl, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            printreptdialogContentPane.add(panel3, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 5, 0), 0, 0));
+
+            //======== panel4 ========
+            {
+                panel4.setLayout(new GridBagLayout());
+                ((GridBagLayout)panel4.getLayout()).columnWidths = new int[] {0, 0, 0};
+                ((GridBagLayout)panel4.getLayout()).rowHeights = new int[] {0, 0};
+                ((GridBagLayout)panel4.getLayout()).columnWeights = new double[] {1.0, 1.0, 1.0E-4};
+                ((GridBagLayout)panel4.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
+
+                //---- okreptbtn ----
+                okreptbtn.setText("\u0414\u0430");
+                okreptbtn.addActionListener(e -> okreptbtnActionPerformed());
+                panel4.add(okreptbtn, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- noreptbtn ----
+                noreptbtn.setText("\u041d\u0435\u0442");
+                noreptbtn.addActionListener(e -> noreptbtnActionPerformed());
+                panel4.add(noreptbtn, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            printreptdialogContentPane.add(panel4, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(0, 0, 0, 0), 0, 0));
+            printreptdialog.setSize(325, 135);
+            printreptdialog.setLocationRelativeTo(printreptdialog.getOwner());
+        }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Gevtsi Yurii
+    // Generated using JFormDesigner Evaluation license - Yurii
     private JTextField logintextField;
     private JPasswordField passwordField;
     private JButton loggBtn;
     private JDialog newworkdialog;
+    private JDialog moneydialog;
+    private JPanel panel1;
+    private JLabel moneylbl;
+    private JPanel panel2;
+    private JDialog printreptdialog;
+    private JPanel panel3;
+    private JLabel reportlbl;
+    private JPanel panel4;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
