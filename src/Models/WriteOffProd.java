1cc26657cac8e717cb7c3ba4ffc097ab62493fe1
Yurii Gevtsi <y.gevtsi@gmail.com>
2018-08-19 17:22:02 +0200

    ReturnProd

--- src/Models/WriteOffProd.java
+++ src/Models/WriteOffProd.java
@@ -5,10 +5,8 @@
 package Models;
 
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.awt.event.*;
+import java.sql.*;
 
 import javax.swing.*;
 import net.miginfocom.swing.*;
@@ -43,11 +41,14 @@ class WriteOffProd extends JFrame {
 							+ codetextField.getText() + " )-" + counttextField.getText() + ")   WHERE \"idProduct\" = \'"
 							+ codetextField.getText() + "\'");
 			pr.execute();
-			pr = cn.prepareStatement(
-					"insert into \"Writen_Off_Product\" (\"idWrittenOffProduct\",\"reason\",\"idProduct\",\"amount\")\n"
-							+ "Values("+id+","+reasontextArea.getText()+","+codetextField.getText()+","+counttextField.getText()+")");
+			pr = cn.prepareStatement("insert into \"Writen_Off_Product\" (\"idWrittenOffProduct\", date, reason, amount, \"idProduct\") values (?,?,?,?,?)");
+			pr.setInt(1,id);
+			pr.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+			pr.setString(3,reasontextArea.getText());
+			pr.setInt(4,Integer.parseInt(counttextField.getText()));
+			pr.setInt(5,Integer.parseInt(codetextField.getText()));
+			pr.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -59,169 +60,214 @@ class WriteOffProd extends JFrame {
 		writeoffdialog.dispose();
 	}
 
-	private void initComponents() {
-		// JFormDesigner - Component initialization - DO NOT MODIFY
-		// //GEN-BEGIN:initComponents
-		// Generated using JFormDesigner Evaluation license - Gevtsi Yurii
-		// JFormDesigner - Variables declaration - DO NOT MODIFY //GEN-BEGIN:variables
-		// Generated using JFormDesigner Evaluation license - Gevtsi Yurii
-		JLabel namelabel = new JLabel();
-		nametextField = new JTextField();
-		JLabel codelabel = new JLabel();
-		codetextField = new JTextField();
-		JLabel countlabel = new JLabel();
-		counttextField = new JTextField();
-		JLabel reasonlabel = new JLabel();
-		JScrollPane scrollPane1 = new JScrollPane();
-		reasontextArea = new JTextArea();
-		JPanel panel1 = new JPanel();
-		JButton okbtn = new JButton();
-		writeoffdialog = new JDialog();
-		JPanel panel2 = new JPanel();
-		JLabel label4 = new JLabel();
-		masseglabel = new JLabel();
-		JPanel panel3 = new JPanel();
-		JButton okbtn2 = new JButton();
-		JButton nobtn2 = new JButton();
+    private void exitbtnActionPerformed() {
+        this.dispose();
+    }
 
-		// ======== this ========
-		setTitle("\u0421\u043f\u0438\u0441\u0430\u0442\u044c \u0442\u043e\u0432\u0430\u0440");
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new MigLayout("hidemode 3",
-				// columns
-				"[4,fill]" + "[87,grow,fill]" + "[165,grow,fill]",
-				// rows
-				"[]" + "[40,grow,fill]" + "[42,grow,fill]" + "[46,grow,fill]" + "[95,grow,fill]" + "[39,grow,fill]"));
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Yurii
+        namelabel = new JLabel();
+        nametextField = new JTextField();
+        codelabel = new JLabel();
+        codetextField = new JTextField();
+        countlabel = new JLabel();
+        counttextField = new JTextField();
+        reasonlabel = new JLabel();
+        scrollPane1 = new JScrollPane();
+        reasontextArea = new JTextArea();
+        panel1 = new JPanel();
+        okbtn = new JButton();
+        var exitbtn = new JButton();
+        writeoffdialog = new JDialog();
+        panel2 = new JPanel();
+        label4 = new JLabel();
+        masseglabel = new JLabel();
+        panel3 = new JPanel();
+        okbtn2 = new JButton();
+        nobtn2 = new JButton();
 
-		// ---- namelabel ----
-		namelabel.setText("\u041d\u0430\u0437\u0432\u0430\u043d\u0438\u0435");
-		contentPane.add(namelabel, "cell 1 1,alignx center,growx 0");
-		contentPane.add(nametextField, "");
+        //======== this ========
+        setTitle("\u0421\u043f\u0438\u0441\u0430\u0442\u044c \u0442\u043e\u0432\u0430\u0440");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        var contentPane = getContentPane();
+        contentPane.setLayout(new GridBagLayout());
+        ((GridBagLayout)contentPane.getLayout()).columnWidths = new int[] {14, 118, 206, 0};
+        ((GridBagLayout)contentPane.getLayout()).rowHeights = new int[] {0, 59, 62, 67, 128, 49, 0};
+        ((GridBagLayout)contentPane.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0, 1.0E-4};
+        ((GridBagLayout)contentPane.getLayout()).rowWeights = new double[] {0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0E-4};
 
-		// ---- codelabel ----
-		codelabel.setText("\u041a\u043e\u0434 \u0442\u043e\u0432\u0430\u0440\u0430");
-		contentPane.add(codelabel, "cell 1 2,alignx center,growx 0");
-		contentPane.add(codetextField, "cell 2 2");
+        //---- namelabel ----
+        namelabel.setText("\u041d\u0430\u0437\u0432\u0430\u043d\u0438\u0435");
+        contentPane.add(namelabel, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+            new Insets(0, 0, 9, 9), 0, 0));
+        contentPane.add(nametextField, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 9, 0), 0, 0));
 
-		// ---- countlabel ----
-		countlabel.setText("\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e");
-		contentPane.add(countlabel, "cell 1 3,alignx center,growx 0");
-		contentPane.add(counttextField, "cell 2 3");
+        //---- codelabel ----
+        codelabel.setText("\u041a\u043e\u0434 \u0442\u043e\u0432\u0430\u0440\u0430");
+        contentPane.add(codelabel, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+            new Insets(0, 0, 9, 9), 0, 0));
+        contentPane.add(codetextField, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 9, 0), 0, 0));
 
-		// ---- reasonlabel ----
-		reasonlabel.setText("\u041f\u0440\u0438\u0447\u0438\u043d\u0430");
-		contentPane.add(reasonlabel, "cell 1 4,alignx center,growx 0");
+        //---- countlabel ----
+        countlabel.setText("\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e");
+        contentPane.add(countlabel, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+            new Insets(0, 0, 9, 9), 0, 0));
+        contentPane.add(counttextField, new GridBagConstraints(2, 3, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 9, 0), 0, 0));
 
-		// ======== scrollPane1 ========
-		{
-			scrollPane1.setMinimumSize(new Dimension(16, 76));
+        //---- reasonlabel ----
+        reasonlabel.setText("\u041f\u0440\u0438\u0447\u0438\u043d\u0430");
+        contentPane.add(reasonlabel, new GridBagConstraints(1, 4, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+            new Insets(0, 0, 9, 9), 0, 0));
 
-			// ---- reasontextArea ----
-			reasontextArea.setLineWrap(true);
-			scrollPane1.setViewportView(reasontextArea);
-		}
-		contentPane.add(scrollPane1, "cell 2 4");
+        //======== scrollPane1 ========
+        {
+            scrollPane1.setMinimumSize(new Dimension(16, 76));
 
-		// ======== panel1 ========
-		{
+            //---- reasontextArea ----
+            reasontextArea.setLineWrap(true);
+            scrollPane1.setViewportView(reasontextArea);
+        }
+        contentPane.add(scrollPane1, new GridBagConstraints(2, 4, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 9, 0), 0, 0));
 
-			// JFormDesigner evaluation mark
-			panel1.setBorder(new javax.swing.border.CompoundBorder(
-					new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0), "",
-							javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.BOTTOM,
-							new java.awt.Font("Dialog", java.awt.Font.BOLD, 12), java.awt.Color.red),
-					panel1.getBorder()));
-			panel1.addPropertyChangeListener(e -> {
-				if ("border".equals(e.getPropertyName()))
-					throw new RuntimeException();
-			});
+        //======== panel1 ========
+        {
+            panel1.setLayout(new MigLayout(
+                "hidemode 3",
+                // columns
+                "[fill]" +
+                "[grow,fill]" +
+                "[fill]" +
+                "[52,fill]" +
+                "[fill]" +
+                "[grow,fill]",
+                // rows
+                "[42,grow,fill]" +
+                "[grow,fill]" +
+                "[]" +
+                "[]"));
 
-			panel1.setLayout(new MigLayout("hidemode 3",
-					// columns
-					"[fill]" + "[fill]" + "[52,fill]",
-					// rows
-					"[fill]" + "[grow,fill]" + "[]" + "[]"));
+            //---- okbtn ----
+            okbtn.setText("\u0421\u043f\u0438\u0441\u0430\u0442\u044c");
+            okbtn.setHorizontalAlignment(SwingConstants.LEFT);
+            okbtn.addActionListener(e -> okbtnActionPerformed());
+            panel1.add(okbtn, "cell 1 0");
 
-			// ---- okbtn ----
-			okbtn.setText("\u0421\u043f\u0438\u0441\u0430\u0442\u044c");
-			okbtn.setHorizontalAlignment(SwingConstants.LEFT);
-			okbtn.addActionListener(e -> okbtnActionPerformed());
-			panel1.add(okbtn, "cell 1 0 2 2");
-		}
-		contentPane.add(panel1, "cell 2 5");
-		setSize(570, 375);
-		setLocationRelativeTo(getOwner());
+            //---- exitbtn ----
+            exitbtn.setText("\u0412\u044b\u0445\u043e\u0434");
+            exitbtn.addActionListener(e -> exitbtnActionPerformed());
+            panel1.add(exitbtn, "cell 5 0");
+        }
+        contentPane.add(panel1, new GridBagConstraints(2, 5, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 0), 0, 0));
+        setSize(570, 375);
+        setLocationRelativeTo(getOwner());
 
-		// ======== writeoffdialog ========
-		{
-			writeoffdialog.setAlwaysOnTop(true);
-			writeoffdialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-			writeoffdialog.setResizable(false);
-			Container writeoffdialogContentPane = writeoffdialog.getContentPane();
-			writeoffdialogContentPane.setLayout(new GridLayout(2, 1));
+        //======== writeoffdialog ========
+        {
+            writeoffdialog.setAlwaysOnTop(true);
+            writeoffdialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            writeoffdialog.setResizable(false);
+            var writeoffdialogContentPane = writeoffdialog.getContentPane();
+            writeoffdialogContentPane.setLayout(new GridLayout(2, 1));
 
-			// ======== panel2 ========
-			{
+            //======== panel2 ========
+            {
+                panel2.setLayout(new MigLayout(
+                    "hidemode 3",
+                    // columns
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]" +
+                    "[]"));
 
-				// JFormDesigner evaluation mark
-				panel2.setBorder(new javax.swing.border.CompoundBorder(
-						new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0), "",
-								javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.BOTTOM,
-								new java.awt.Font("Dialog", java.awt.Font.BOLD, 12), java.awt.Color.red),
-						panel2.getBorder()));
-				panel2.addPropertyChangeListener(e -> {
-					if ("border".equals(e.getPropertyName()))
-						throw new RuntimeException();
-				});
+                //---- label4 ----
+                label4.setText("\u041f\u043e\u0434\u0442\u0432\u0435\u0440\u0434\u0438\u0442\u044c \u0441\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u043e\u0432\u0430\u0440\u043e\u0432 :");
+                panel2.add(label4, "cell 2 1");
 
-				panel2.setLayout(new MigLayout("hidemode 3",
-						// columns
-						"[fill]" + "[fill]" + "[fill]" + "[fill]" + "[fill]" + "[fill]" + "[fill]",
-						// rows
-						"[]" + "[]" + "[]" + "[]" + "[]" + "[]" + "[]"));
+                //---- masseglabel ----
+                masseglabel.setText("text");
+                panel2.add(masseglabel, "cell 2 2");
+            }
+            writeoffdialogContentPane.add(panel2);
 
-				// ---- label4 ----
-				label4.setText(
-						"\u041f\u043e\u0434\u0442\u0432\u0435\u0440\u0434\u0438\u0442\u044c \u0441\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u043e\u0432\u0430\u0440\u043e\u0432 :");
-				panel2.add(label4, "cell 2 1");
+            //======== panel3 ========
+            {
+                panel3.setLayout(new MigLayout(
+                    "hidemode 3",
+                    // columns
+                    "[22,fill]" +
+                    "[fill]" +
+                    "[fill]" +
+                    "[34,fill]" +
+                    "[37,fill]" +
+                    "[fill]" +
+                    "[fill]",
+                    // rows
+                    "[]" +
+                    "[]" +
+                    "[]"));
 
-				// ---- masseglabel ----
-				masseglabel.setText("text");
-				panel2.add(masseglabel, "cell 2 2");
-			}
-			writeoffdialogContentPane.add(panel2);
+                //---- okbtn2 ----
+                okbtn2.setText("\u0414\u0430");
+                okbtn2.addActionListener(e -> okbtn2ActionPerformed());
+                panel3.add(okbtn2, "cell 2 1");
 
-			// ======== panel3 ========
-			{
-				panel3.setLayout(new MigLayout("hidemode 3",
-						// columns
-						"[22,fill]" + "[fill]" + "[fill]" + "[34,fill]" + "[37,fill]" + "[fill]" + "[fill]",
-						// rows
-						"[]" + "[]" + "[]"));
+                //---- nobtn2 ----
+                nobtn2.setText("\u041d\u0435\u0442");
+                nobtn2.addActionListener(e -> nobtn2ActionPerformed());
+                panel3.add(nobtn2, "cell 4 1");
+            }
+            writeoffdialogContentPane.add(panel3);
+            writeoffdialog.setSize(280, 160);
+            writeoffdialog.setLocationRelativeTo(writeoffdialog.getOwner());
+        }
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
 
-				// ---- okbtn2 ----
-				okbtn2.setText("\u0414\u0430");
-				okbtn2.addActionListener(e -> okbtn2ActionPerformed());
-				panel3.add(okbtn2, "cell 2 1");
-
-				// ---- nobtn2 ----
-				nobtn2.setText("\u041d\u0435\u0442");
-				nobtn2.addActionListener(e -> nobtn2ActionPerformed());
-				panel3.add(nobtn2, "cell 4 1");
-			}
-			writeoffdialogContentPane.add(panel3);
-			writeoffdialog.setSize(280, 160);
-			writeoffdialog.setLocationRelativeTo(writeoffdialog.getOwner());
-		}
-		// JFormDesigner - End of component initialization //GEN-END:initComponents
-	}
-
-	private JTextField nametextField;
-	private JDialog writeoffdialog;
-	private JLabel masseglabel;
-	private JTextField counttextField;
-	private JTextField codetextField;
-	private JTextArea reasontextArea;
-	// JFormDesigner - End of variables declaration //GEN-END:variables
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Yurii
+    private JLabel namelabel;
+    private JTextField nametextField;
+    private JLabel codelabel;
+    private JTextField codetextField;
+    private JLabel countlabel;
+    private JTextField counttextField;
+    private JLabel reasonlabel;
+    private JScrollPane scrollPane1;
+    private JTextArea reasontextArea;
+    private JPanel panel1;
+    private JButton okbtn;
+    private JDialog writeoffdialog;
+    private JPanel panel2;
+    private JLabel label4;
+    private JLabel masseglabel;
+    private JPanel panel3;
+    private JButton okbtn2;
+    private JButton nobtn2;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
